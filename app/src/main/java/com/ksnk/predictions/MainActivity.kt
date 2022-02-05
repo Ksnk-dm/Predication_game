e885438a37723a36fdaa7e01d5faf0b329db52b2
Dmitry Kosenko <71468576+Ksnk-dm@users.noreply.github.com>
2022-02-05 14:17:34 +0200

    fix

--- app/src/main/java/com/ksnk/predictions/MainActivity.kt
+++ app/src/main/java/com/ksnk/predictions/MainActivity.kt
@@ -1,30 +1,25 @@
 package com.ksnk.predictions
 
 
-import android.app.Notification
-import android.app.NotificationManager
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationManagerCompat
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.OnUserEarnedRewardListener
+import com.google.android.gms.ads.*
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.android.gms.ads.rewarded.RewardItem
 import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
 import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAdLoadCallback
 import com.ksnk.predictions.dao.PredicationDao
-import org.w3c.dom.Text
 import com.ksnk.predictions.entity.Predication as Predication1
 
 
@@ -33,48 +28,14 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
     private var predicationImageView2: ImageView? = null
     private var predicationImageView3: ImageView? = null
     private var countTextView: TextView? = null
-    private var timerTextView: TextView? = null
-    private var count = 0
+    private var plusTextView: TextView? = null
+    private var count = 3
     private var countTemp = 0
-
-
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "MainActivity"
+    lateinit var mAdView : AdView
     var statusFirstRun = true
-    private var receiver: TimerStatusReceiver? = null
 
-
-    private val predicationList = listOf(
-        "Не жди чуда – чуди сам.",
-        "Мир принадлежит тому, кто ему рад.",
-        "Поздравляем! Вы находитесь на верном пути.",
-        "Покорив одну гору, начинай штурмовать другую...",
-        "Прилив энергии поможет Вам справиться с большим объемом незапланированных работ.",
-        "Примите то, что вы не можете изменить, и вы будете чувствовать себя лучше.",
-        "Природа, время и терпение - три великих врача.", "Пришло время действовать!",
-        "Пришло время закончить старое и начать новое.",
-        "Пусть мир наполнится спокойствием и доброжелательностью",
-        "Работа с новыми партнерами будет очень выгодным.",
-        "Работайте над дипломатическими способностями - они очень пригодятся для реализации идей.",
-        "Размышляйте и не спешите с действиями.",
-        "Разрешите состраданию направлять ваши решения.",
-        "Результат Ваших действий может оказаться неожиданным.",
-        "Романтика переместит вас в новом направлении.",
-        "С этого момента ваша доброта приведет вас к успеху.",
-        "Сегодня у вас будет красивый день.",
-        "Семь раз отмерьте, один раз отрежьте!",
-        "Слушайте каждого. Идеи приходят отовсюду.",
-        "Соловья баснями не кормят.",
-        "Сосредоточьтесь на семье и гармонии с окружающим миром.",
-        "Счастливая жизнь прямо перед вами.",
-        "Теперь настало время попробовать что-то новое.",
-        "Терпение! Вы почти у цели.",
-        "Тот, кто знает,  достаточно богат.",
-        "Тот, кто не ждет благодарности, никогда не будет разочарован.",
-        "Удача проводит Вас через все трудные времена.",
-        "Пусть мир наполнится спокойствием и доброжелательностью",
-        "Пусть мир наполнится спокойствием и доброжелательностью",
-        "Пусть мир наполнится спокойствием и доброжелательностью"
-
-    )
 
     private var rewardedInterstitialAd: RewardedInterstitialAd? = null
 
@@ -84,8 +45,8 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
         editor: SharedPreferences.Editor
     ) {
         if (statusFirstRun) {
-            for (i in predicationList.indices) {
-                val predication = Predication1(predicationList.get(i))
+            for (i in Contains.predicationList.indices) {
+                val predication = Predication1(Contains.predicationList[i])
                 userDao.insertAll(predication)
             }
             editor.putBoolean("firstStart", false).apply()
@@ -95,15 +56,16 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
     private fun checkNullSizeList(userDao: PredicationDao, editor: SharedPreferences.Editor) {
         if (userDao.getAll().size < 3) {
             initFirstStart(userDao, true, editor)
+
         }
     }
 
-    private fun checkNullCount(editor: SharedPreferences.Editor) {
+    private fun checkNullCount() {
         if (count == 0) {
             count = 1
-            editor.putInt("count", count).apply()
             predicationImageView1?.visibility = View.VISIBLE
             countTextView?.text = count.toString()
+            plusTextView?.visibility=View.INVISIBLE
         }
     }
 
@@ -114,7 +76,7 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
             // Load an ad.
             RewardedInterstitialAd.load(
                 this,
-                "ca-app-pub-2981423664535117/9800122375",
+                "ca-app-pub-3940256099942544/5354046379",
                 adRequest,
                 object : RewardedInterstitialAdLoadCallback() {
                     override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -138,47 +100,67 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
         rewardedInterstitialAd!!.show(this@MainActivity, this@MainActivity)
     }
 
+    override fun onPause() {
+        super.onPause()
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main)
 
 
-        MobileAds.initialize(this) { initializationStatus -> loadAd() }
 
+
+        MobileAds.initialize(this) { initializationStatus -> loadAd() }
+        mAdView = findViewById(R.id.adView)
+        val adRequestBanner = AdRequest.Builder().build()
+        mAdView.loadAd(adRequestBanner)
         val sharedPreference = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
         var editor = sharedPreference.edit()
         val db: AppDataBase? = App.instance?.getDatabase()
         val userDao: PredicationDao = db!!.predicationDao()
         statusFirstRun = sharedPreference.getBoolean("firstStart", true)
-        count = sharedPreference.getInt("count", 3)
-
+      //  count = sharedPreference.getInt("count", 3)
+Log.d("counter", count.toString())
 
         initFirstStart(userDao, statusFirstRun, editor)
+        var adRequest = AdRequest.Builder().build()
 
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.message)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
 
 
         countTextView = findViewById(R.id.countTextView)
-        timerTextView = findViewById(R.id.timer_text_view)
+        plusTextView = findViewById(R.id.plus_text_view)
         countTextView?.text = count.toString()
-        countTextView?.setOnClickListener {
-            rewardedInterstitialAd?.show(
-                this
-            ) { rewardItem ->
-                checkNullCount(editor)
+        plusTextView ?.setOnClickListener {
+            rewardedInterstitialAd?.show(this) {
+                checkNullCount()
             }
         }
-        receiver = TimerStatusReceiver(timerTextView!!)
+
 
         predicationImageView1 = findViewById(R.id.predictionImageView1)
         predicationImageView1?.setOnClickListener {
             createAlertDialog(userDao, editor)
             count--
-            editor.putInt("count", count).apply()
             countTextView?.text = count.toString()
             predicationImageView1?.visibility = View.INVISIBLE
             if(count==0){
-                startService()
+                plusTextView?.visibility = View.VISIBLE
             }
 
         }
@@ -186,11 +168,10 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
         predicationImageView2?.setOnClickListener {
             createAlertDialog(userDao, editor)
             count--
-            editor.putInt("count", count).apply()
             countTextView?.text = count.toString()
             predicationImageView2?.visibility = View.INVISIBLE
             if(count==0){
-                startService()
+                plusTextView?.visibility = View.VISIBLE
             }
 
         }
@@ -198,68 +179,27 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
         predicationImageView3?.setOnClickListener {
             createAlertDialog(userDao, editor)
             count--
-            editor.putInt("count", count).apply()
             countTextView?.text = count.toString()
             predicationImageView3?.visibility = View.INVISIBLE
             if(count==0){
-                startService()
+                plusTextView?.visibility = View.VISIBLE
             }
 
         }
         if (count == 2) {
             predicationImageView1?.visibility = View.INVISIBLE
-            stopService()
         }
         if (count == 1) {
             predicationImageView1?.visibility = View.INVISIBLE
             predicationImageView2?.visibility = View.INVISIBLE
             predicationImageView3?.visibility = View.VISIBLE
-            stopService()
         }
         if (count == 0) {
             predicationImageView1?.visibility = View.INVISIBLE
             predicationImageView2?.visibility = View.INVISIBLE
             predicationImageView3?.visibility = View.INVISIBLE
-
+            plusTextView?.visibility = View.VISIBLE
         }
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        LocalBroadcastManager.getInstance(this)
-            .registerReceiver(receiver!!, IntentFilter(CountdownTimerService.TIME_INFO))
-    }
-
-    override fun onPause() {
-        super.onPause()
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver!!)
-    }
-
-    private fun startService() {
-        val intent = Intent(this, CountdownTimerService::class.java)
-        startService(intent)
-
-    }
-
-    private fun stopService() {
-        val intent = Intent(this, CountdownTimerService::class.java)
-        stopService(intent)
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        val sharedPreference = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        var editor = sharedPreference.edit()
-        editor.putInt("count", count).apply()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        val sharedPreference = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        var editor = sharedPreference.edit()
-        editor.putInt("count", count).apply()
-
     }
 
     private fun createAlertDialog(userDao: PredicationDao, editor: SharedPreferences.Editor) {
@@ -272,6 +212,18 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
         builder.setMessage(userDao.getAll()[rnds].predicationText)
         id = userDao.getAll()[rnds].uid
         builder.setNegativeButton(android.R.string.no) { dialog, which ->
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            }
+        }
+        builder.setOnCancelListener {
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            }
         }
         userDao.deleteById(id)
         builder.show()
@@ -285,7 +237,7 @@ class MainActivity : AppCompatActivity(), OnUserEarnedRewardListener {
         if (count == 0) {
             count = 1
             predicationImageView1?.visibility = View.VISIBLE
-            stopService()
+            plusTextView?.visibility=View.INVISIBLE
         }
     }
 }
